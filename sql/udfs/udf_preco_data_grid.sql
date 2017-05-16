fd42062ba5f69aa9bc5b6687b96cecae965231e5
Daniela Simões <danielasimoes@ua.pt>
2017-05-16 17:39:06 +0100

    bug fix SQL...

--- sql/udfs/udf_preco_data_grid.sql
+++ sql/udfs/udf_preco_data_grid.sql
@@ -1,4 +1,4 @@
-CREATE FUNCTION db.udf_preco_data_grid(@nome varchar(30))
+CREATE FUNCTION db.udf_preco_data_grid(@nome varchar(30)=null)
 RETURNS @table TABLE ("Name" varchar(30), "Lab" int, "PVP" int, "Price" int, "IVA" int)
 
 WITH SCHEMABINDING, ENCRYPTION
@@ -12,11 +12,8 @@ BEGIN
 		END;
 	ELSE
 		BEGIN
-		IF (@nome is not null)
-			BEGIN
-				INSERT @table SELECT Vende.medicamento_nome, Vende.lab_NIPC, Vende.PVP, Vende.Preco, Vende.IVA
-				FROM db.Vende WHERE db.Vende.medicamento_nome = @nome;
-			END;
+			INSERT @table SELECT Vende.medicamento_nome, Vende.lab_NIPC, Vende.PVP, Vende.Preco, Vende.IVA
+			FROM db.Vende WHERE db.Vende.medicamento_nome = @nome;
 		END;
 RETURN;
 END;
