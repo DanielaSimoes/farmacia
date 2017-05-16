154f828e49e39b3f4954799c4de7e1c1c24a0b41
Daniela Simões <danielasimoes@ua.pt>
2017-05-16 17:38:04 +0100

    bug fix of sql split...

--- sql/udfs/stock_data_grid.sql
+++ sql/udfs/stock_data_grid.sql
@@ -1,4 +1,4 @@
-CREATE FUNCTION db.udf_stock_data_grid(@nome varchar(30))
+CREATE FUNCTION db.udf_stock_data_grid(@nome varchar(30)=null)
 RETURNS @table TABLE ("Name" varchar(30), "Lab" int, "Qty" int, "Validity" date, "Dose" int, "Units" int, "Category" int, "Type" int, "Code" int, "PVP" int, "Price" int, "IVA" int, "Prescription" int)
 
 WITH SCHEMABINDING, ENCRYPTION
@@ -12,11 +12,8 @@ BEGIN
 		END;
 	ELSE
 		BEGIN
-			IF (@nome is not null)
-			BEGIN
-	            INSERT @table SELECT Medicamento.nome, Medicamento.lab_id, Medicamento.quantidade, Medicamento.validade, Medicamento.dose, Medicamento.unidades, Medicamento.categoria_id, Medicamento.tipo_id, Medicamento.codigo, Vende.PVP, Vende.Preco, Vende.IVA, NULL
-	            FROM db.Vende JOIN db.Medicamento ON db.Vende.medicamento_nome=db.Medicamento.nome AND db.Vende.lab_NIPC=db.Medicamento.lab_id WHERE db.Medicamento.nome = @nome AND Medicamento.unidades > 0;
-			END;
+	        INSERT @table SELECT Medicamento.nome, Medicamento.lab_id, Medicamento.quantidade, Medicamento.validade, Medicamento.dose, Medicamento.unidades, Medicamento.categoria_id, Medicamento.tipo_id, Medicamento.codigo, Vende.PVP, Vende.Preco, Vende.IVA, NULL
+	        FROM db.Vende JOIN db.Medicamento ON db.Vende.medicamento_nome=db.Medicamento.nome AND db.Vende.lab_NIPC=db.Medicamento.lab_id WHERE db.Medicamento.nome = @nome AND Medicamento.unidades > 0;
 		END;
 RETURN;
 END;
