0275dba695589bc1cf5a0c6669e95a429de5e1e8
Daniela Simões <danielasimoes@ua.pt>
2017-05-30 18:02:52 +0100

    another bug fix

--- sql/udfs/udf_stock_data_code.sql
+++ sql/udfs/udf_stock_data_code.sql
@@ -1,5 +1,5 @@
 CREATE FUNCTION db.udf_stock_data_code(@codigo int)
-RETURNS @table TABLE ("Name" varchar(30), "Lab" int, "Qty" int, "Validity" date, "Dose" int, "Units" int, "Category" int, "Type" int, "Code" int, "PVP" int, "Price" int, "IVA" int, "Prescription" int)
+RETURNS @table TABLE ("Name" varchar(30), "Lab" int, "Qty" int, "Validity" date, "Dose" int, "Category" int, "Type" int, "Code" int, "PVP" int, "Price" int, "IVA" int, "Prescription" int)
 
 WITH SCHEMABINDING, ENCRYPTION
 AS
@@ -7,14 +7,14 @@ AS
 BEGIN
 	IF (@codigo is null)
 		BEGIN
-			INSERT @table SELECT Medicamento.nome, Medicamento.lab_id, Medicamento.quantidade, Medicamento.validade, Medicamento.dose, Medicamento.unidades, Medicamento.categoria_id, Medicamento.tipo_id, Medicamento.codigo, Medicamento.PVP, Medicamento.Preco, Medicamento.IVA, NULL
+			INSERT @table SELECT Medicamento.nome, Medicamento.lab_id, Medicamento.quantidade, Medicamento.validade, Medicamento.dose, Medicamento.categoria_id, Medicamento.tipo_id, Medicamento.codigo, Medicamento.PVP, Medicamento.Preco, Medicamento.IVA, NULL
 			FROM db.Medicamento WHERE Medicamento.unidades > 0;
 		END;
 	ELSE
 		BEGIN
 			IF (@codigo is not null)
 			BEGIN
-	            INSERT @table SELECT Medicamento.nome, Medicamento.lab_id, Medicamento.quantidade, Medicamento.validade, Medicamento.dose, Medicamento.unidades, Medicamento.categoria_id, Medicamento.tipo_id, Medicamento.codigo, Medicamento.PVP, Medicamento.Preco, Medicamento.IVA, NULL
+	            INSERT @table SELECT Medicamento.nome, Medicamento.lab_id, Medicamento.quantidade, Medicamento.validade, Medicamento.dose, Medicamento.categoria_id, Medicamento.tipo_id, Medicamento.codigo, Medicamento.PVP, Medicamento.Preco, Medicamento.IVA, NULL
 	            FROM db.Medicamento WHERE db.Medicamento.codigo = @codigo AND Medicamento.unidades > 0;
 			END;
 		END;
