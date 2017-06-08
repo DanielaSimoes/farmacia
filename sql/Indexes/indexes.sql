a320187f56b726ffbe5810530cf849066fe4dba1
Cristiana Carvalho <cristiana96carvalho@hotmail.com>
2017-06-08 13:43:50 +0100

    indices

---
+++ sql/Indexes/indexes.sql
@@ -0,0 +1,2 @@
+CREATE INDEX cat on db.Medicamento(categoria_id);
+CREATE INDEX tipo on db.Medicamento(tipo_id);
--- sql/Indexes/indexes_nif.sql
+++
@@ -1,8 +0,0 @@
-CREATE INDEX indexnif_utente ON db.Utente(NIF)
-
-go
-
-
-CREATE INDEX indexnif_func ON db.Funcionario(NIF)
-
-go
